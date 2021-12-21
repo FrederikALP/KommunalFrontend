9b7fac764446d433704241af868588a4bd6989d7
FrederikALP <fred05k3@stud.kea.dk>
2021-12-21 05:02:33 +0100

    sql resources

---
+++ resources/kommunalvalg_exam_parties.sql
@@ -0,0 +1,11 @@
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (1, 'A - Socialdemokratiet', 4351);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (2, 'B - Radikale Venstre', 3121);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (3, 'C - Det Konservative Folkeparti', 23506);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (4, 'D - Nye Borgerlige', 590);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (5, 'F - SF - Socialistisk Folkeparti', 1231);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (6, 'I - Liberal Alliance', 757);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (7, 'K - Kristendemokraterne', 86);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (8, 'O - Dansk Folkeparti', 812);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (9, 'V - Venstre, Danmarks Liberale Parti', 2649);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (10, 'Ø - Enhedslisten - De Rød-Grønne', 2147);
+INSERT INTO kommunalvalg_exam.parties (id, name, party_votes) VALUES (11, 'Å - Alternativet', 1180);
